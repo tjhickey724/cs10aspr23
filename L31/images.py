ef3fe046e784cca049e3cc2273f657938a077c1c
tim <tjhickey@brandeis.edu>
2023-04-19 08:48:55 -0400

    updated images in L31, added L32

--- L31/images.py
+++ L31/images.py
@@ -10,6 +10,34 @@ def shrink(img,k):
     print(dw,dh)
     return img[::dw,::dh,::]
 
+def redfilter1(img):
+    img2 = img.copy()
+    print('copied')
+    for i in range(len(img2)):
+        if i%100==0:
+            print(i,end=" ")
+        for j in range(len(img2[0])):
+            x = img2[i][j]
+            m = (int(x[0]) + int(x[1]) + int(x[2]))//3
+            x[0]=m
+            x[1]=0
+            x[2]=0
+    print('modified')
+    return img2
+
+def redfilter(img):
+    img2 = img.copy()
+    print('copied')
+    for i in range(len(img2)):
+        if i%100==0:
+            print(i,end=" ")
+        for j in range(len(img2[0])):
+            x = img2[i][j]
+            x[1]=0
+            x[2]=0
+    print('modified')
+    return img2
+
 def grayscale(img):
     img2 = img.copy()
     print('copied')
@@ -30,13 +58,15 @@ def main():
     from matplotlib import image
     import numpy as np
 
-    image2 = image.imread('L31/images/coffeecup.jpeg')
+    image2 = image.imread('L31/images/love.jpg')
 
     img = np.asarray(image2)
-    print(img[100][100])
-    print(img.shape)
-    print(img.dtype)
-    pyplot.imshow(image2)
+    img2 = img[::10,::10,::]
+    print(img2[100][100])
+    print(img2.shape)
+    print(img2.dtype)
+    redimage = grayscale(img2)
+    pyplot.imshow(img)
     pyplot.show()
 
 main()
